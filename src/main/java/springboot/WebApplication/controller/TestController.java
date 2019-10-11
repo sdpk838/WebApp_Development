603fd9120803e309333ab811535019d3f368a8d0
Ravi Chandrika <Ravi.Chandrika@cai.io>
2019-10-11 13:21:57 +0530

    WebChanges

---
+++ src/main/java/springboot/WebApplication/controller/TestController.java
@@ -0,0 +1,12 @@
+package springboot.WebApplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello blueGreen Vacations";
+	}
+}
